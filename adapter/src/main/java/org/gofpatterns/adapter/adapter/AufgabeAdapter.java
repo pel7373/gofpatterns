cc144e98bd40ef3641003cfacbddf7465412209d
Paul Liebiediev <pel7373@gmail.com>
2024-09-25 22:01:26 +0300

    adapter: two-way adapter via interfaces was implemented

--- adapter/src/main/java/org/gofpatterns/adapter/adapter/AufgabeAdapter.java
+++
@@ -1,8 +0,0 @@
-package org.gofpatterns.adapter.adapter;
-
-public interface AufgabeAdapter {
-    String getTitle();
-    void setTitle(String title);
-    String getDescription();
-    void setDescription(String description);
-}
--- adapter/src/main/java/org/gofpatterns/adapter/germanTask/AufgabeImpl.java
+++ adapter/src/main/java/org/gofpatterns/adapter/germanTask/AufgabeImpl.java
@@ -1,21 +1,33 @@
-package org.gofpatterns.adapter.germanClass;
+package org.gofpatterns.adapter.germanTask;
 
-public class Aufgabe {
+public class AufgabeImpl implements Aufgabe {
     private String titel;
     private String beschreibung;
 
+    public AufgabeImpl() {
+    }
+
+    public AufgabeImpl(String titel, String beschreibung) {
+        this.titel = titel;
+        this.beschreibung = beschreibung;
+    }
+
+    @Override
     public String titelErhalten() {
       return titel;
     }
 
+    @Override
     public void titelFestlegen(String titel) {
         this.titel = titel;
     }
 
+    @Override
     public String beschreibungAbrufen() {
         return beschreibung;
     }
 
+    @Override
     public void beschreibungFestlegen(String beschreibung) {
         this.beschreibung = beschreibung;
     }
